f3ad216db759533b030b08f184b586b16b4bd4cf
Muhamad Akbar Afriansyah <itsme@akbarafriansyah.my.id>
2024-11-22 09:41:11 +0000

    move components

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react';
-import Modal from '../../components/Modal';
+import Modal from '../components/Modal';
 
 export default function SignupPage() {
   const [email, setEmail] = useState('');
